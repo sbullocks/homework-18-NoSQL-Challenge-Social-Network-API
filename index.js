19917ae0c14019be483edfb2b58017ad6a044611
sbullocks <sbullocks@gmail.com>
2022-11-24 14:17:56 -0500

    fixed syntax.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const PORT = process.env.port || 3001;
 const app = express();
@@ -9,17 +9,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/aggregateDB', {
+mongoose.connect("mongodb://127.0.0.1:27017/aggregateDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Export connection 
+// Export connection
 module.exports = mongoose.connection;
 
 app.listen(PORT, () => {
-    console.log(`Connecting to port ${PORT}!`);
-  });
+  console.log(`Connecting to port ${PORT}!`);
+});
