e0514c296c5cb8bb753edb6114226f43fd7dc556
sbullocks <sbullocks@gmail.com>
2022-11-25 14:45:06 -0500

    defined routes on index.js file.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtsRoutes = require('./thoughts-routes');
 const userRoutes = require('./users-routes');
 
-router.use('/thoughts', thoughts-routes);
-router.use('/users', users-routes);
+router.use('/thoughts', thoughtsRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
