4c9281dd80fd14d311702dc6cc236f34077bb449
sbullocks <sbullocks@gmail.com>
2022-11-25 14:11:53 -0500

    updated seed.js file

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
-const { Post, Tags } = require('../models');
+const { Thoughts, Users } = require('../models');
 // Import functions for seed data
 const { getRandomColor, getRandomPost, genRandomIndex } = require('./data');
 
@@ -9,44 +9,44 @@ console.time('seeding');
 // Creates a connection to mongodb
 connection.once('open', async () => {
   // Delete the entries in the collection
-  await Post.deleteMany({});
-  await Tags.deleteMany({});
+  await Thoughts.deleteMany({});
+  await Users.deleteMany({});
 
-  // Empty arrays for randomly generated posts and tags
-  const tags = [];
-  const posts = [];
+  // Empty arrays for randomly generated Thoughts and Users
+  const tsers = [];
+  const thoughts = [];
 
-  // Function to make a post object and push it into the posts array
-  const makePost = (text) => {
-    posts.push({
+  // Function to make a Thought object and push it into the Thoughts array
+  const makeThoughts = (text) => {
+    thoughts.push({
       published: Math.random() < 0.5,
       text,
-      tags: [tags[genRandomIndex(tags)]._id],
+      users: [users[genRandomIndex(users)]._id],
     });
   };
 
-  // Create 20 random tags and push them into the tags array
+  // Create 20 random users and push them into the users array
   for (let i = 0; i < 20; i++) {
     const tagname = getRandomColor();
 
-    tags.push({
+    users.push({
       tagname,
       color: tagname,
     });
   }
 
-  // Wait for the tags to be inserted into the database
-  await Tags.collection.insertMany(tags);
+  // Wait for the users to be inserted into the database
+  await Users.collection.insertMany(users);
 
-  // For each of the tags that exist, make a random post of length 50
-  tags.forEach(() => makePost(getRandomPost(50)));
+  // For each of the users that exist, make a random thought of length 50
+  users.forEach(() => makeThoughts(getRandomThoughts(50)));
 
-  // Wait for the posts array to be inserted into the database
-  await Post.collection.insertMany(posts);
+  // Wait for the Thoughts array to be inserted into the database
+  await Thoughts.collection.insertMany(thoughts);
 
-  // Log out a pretty table for tags and posts, excluding the excessively long text property
-  console.table(tags);
-  console.table(posts, ['published', 'tags', '_id']);
+  // Log out a pretty table for users and Thoughts, excluding the excessively long text property
+  console.table(users);
+  console.table(thoughts, ['published', 'users', '_id']);
   console.timeEnd('seeding');
   process.exit(0);
 });
