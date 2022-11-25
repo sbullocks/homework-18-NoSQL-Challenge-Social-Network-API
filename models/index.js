fdf9f3cfe6e13eaa70589bd9e950a7860362ca4d
sbullocks <sbullocks@gmail.com>
2022-11-25 12:36:28 -0500

    updated index.js file for models.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const Thoughts = require('./Thoughts');
-const Users = require('./Users');
+const Thought = require('./Thought');
+const User = require('./User');
 
-module.exports = { Thoughts, Users };
+module.exports = { Thought, User };
