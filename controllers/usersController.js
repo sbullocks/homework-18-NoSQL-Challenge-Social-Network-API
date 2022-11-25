b590eae212213ec4d728a8b6fe42bb32ccac00e4
sbullocks <sbullocks@gmail.com>
2022-11-25 13:40:29 -0500

    updated usersController.js file.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,42 +1,55 @@
-const { Thought, User } = require('../models');
+const { User, User } = require('../models');
 
 module.exports = {
-  getTags(req, res) {
-    Tags.find({})
-      .select('-__v')
-      .then((tags) => res.json(tags))
+  // Get all Users
+  getUsers(req, res) {
+    User.find()
+      .then((Users) => res.json(Users))
       .catch((err) => res.status(500).json(err));
   },
-  getSingleTag(req, res) {
-    Tags.findOne({ _id: req.params.tagId })
+  // Get a User
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.UserId })
       .select('-__v')
-      .then((tag) =>
-        !tag
-          ? res.status(404).json({ message: 'No tag with that ID' })
-          : res.json(tag)
+      .then((User) =>
+        !User
+          ? res.status(404).json({ message: 'No User with that ID' })
+          : res.json(User)
       )
       .catch((err) => res.status(500).json(err));
   },
-  // create a new tag
-  createTag(req, res) {
-    Tags.create(req.body)
-      .then((tag) => {
-        return Post.findOneAndUpdate(
-          { _id: req.body.postId },
-          { $addToSet: { tags: tag._id } },
-          { new: true }
-        );
-      })
-      .then((user) =>
-        !user
-          ? res
-              .status(404)
-              .json({ message: 'Tag created, but found no post with that ID' })
-          : res.json('Created the tag 🎉')
-      )
+  // Create a User
+  createUser(req, res) {
+    User.create(req.body)
+      .then((User) => res.json(User))
       .catch((err) => {
         console.log(err);
-        res.status(500).json(err);
+        return res.status(500).json(err);
       });
+  },
+  // Delete a User
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.UserId })
+      .then((User) =>
+        !User
+          ? res.status(404).json({ message: 'No User with that ID' })
+          : Student.deleteMany({ _id: { $in: User.students } })
+      )
+      .then(() => res.json({ message: 'User and students deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a User
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.UserId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((User) =>
+        !User
+          ? res.status(404).json({ message: 'No User with this id!' })
+          : res.json(User)
+      )
+      .catch((err) => res.status(500).json(err));
   },
 };
