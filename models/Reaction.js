c288fd18f915749734948cc5b76aca21cb818ec9
sbullocks <sbullocks@gmail.com>
2022-11-25 15:39:10 -0500

    updated readme with task list of completion items still needed.

--- models/Reaction.js
+++ models/Reaction.js
@@ -19,7 +19,7 @@ const ReactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+      get: (createdAtVal) => date(createdAtVal),
     },
   },
   {
