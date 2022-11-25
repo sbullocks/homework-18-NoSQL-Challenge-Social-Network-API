3f6a127e436f1f2ce773fcd60d2dd893257fe1ee
sbullocks <sbullocks@gmail.com>
2022-11-25 14:17:41 -0500

    updated seed.js file.

--- utils/seed.js
+++ utils/seed.js
@@ -1,7 +1,7 @@
 const connection = require('../config/connection');
 const { Thoughts, Users } = require('../models');
 // Import functions for seed data
-const { getRandomColor, getRandomPost, genRandomIndex } = require('./data');
+const { users, thoughts } = require('./data');
 
 // Start the seeding runtime timer
 console.time('seeding');
@@ -13,33 +13,33 @@ connection.once('open', async () => {
   await Users.deleteMany({});
 
   // Empty arrays for randomly generated Thoughts and Users
-  const tsers = [];
+  const users = [];
   const thoughts = [];
 
   // Function to make a Thought object and push it into the Thoughts array
-  const makeThoughts = (text) => {
-    thoughts.push({
-      published: Math.random() < 0.5,
-      text,
-      users: [users[genRandomIndex(users)]._id],
-    });
-  };
+  // const makeThoughts = (text) => {
+  //   thoughts.push({
+  //     published: Math.random() < 0.5,
+  //     text,
+  //     users: [users[genRandomIndex(users)]._id],
+  //   });
+  // };
 
   // Create 20 random users and push them into the users array
-  for (let i = 0; i < 20; i++) {
-    const tagname = getRandomColor();
+  // for (let i = 0; i < 20; i++) {
+  //   const tagname = getRandomColor();
 
-    users.push({
-      tagname,
-      color: tagname,
-    });
-  }
+  //   users.push({
+  //     tagname,
+  //     color: tagname,
+  //   });
+  // }
 
   // Wait for the users to be inserted into the database
   await Users.collection.insertMany(users);
 
   // For each of the users that exist, make a random thought of length 50
-  users.forEach(() => makeThoughts(getRandomThoughts(50)));
+  // users.forEach(() => makeThoughts(getRandomThoughts(50)));
 
   // Wait for the Thoughts array to be inserted into the database
   await Thoughts.collection.insertMany(thoughts);
