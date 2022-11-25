f0845ef54af686b31c7159fe51bfda45128b9124
sbullocks <sbullocks@gmail.com>
2022-11-25 14:39:17 -0500

    fixed users routes on index.js file.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const thoughtsRoutes = require('./thoughts-routes');
-const userRoutes = require('./user-routes');
+const userRoutes = require('./users-routes');
 
 router.use('/thoughts', thoughts-routes);
-router.use('/users', user-routes);
+router.use('/users', users-routes);
 
 module.exports = router;
