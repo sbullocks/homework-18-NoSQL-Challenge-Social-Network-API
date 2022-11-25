7503a21abefb9d3712c35a5e042926cf1a0ebec5
sbullocks <sbullocks@gmail.com>
2022-11-25 14:49:21 -0500

    removed duplicate mongoose connection from index.js file as it already defined in connection.js file.

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/aggregateDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://127.0.0.1:27017/aggregateDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 // Export connection
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
 
 app.listen(PORT, () => {
   console.log(`Connecting to port ${PORT}!`);
