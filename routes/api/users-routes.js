57c66ae71c1eb011183ae8f2058cef0ba315d5bc
sbullocks <sbullocks@gmail.com>
2022-11-26 19:48:16 -0500

    added routes for updating/deleting users.

--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -7,13 +7,13 @@ const {
   deleteUser,
   addFriend,
   removeFriend,
-  
+
 } = require('../../controllers/usersController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
