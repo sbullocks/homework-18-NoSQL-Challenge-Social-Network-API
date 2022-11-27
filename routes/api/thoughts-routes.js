871dee164053dfc33c11325fb2aa79ba3a6eee38
sbullocks <sbullocks@gmail.com>
2022-11-26 19:38:30 -0500

    updated route for thoughtId to add reactions.

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -14,4 +14,6 @@ router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+router.route('/:thoughtId/reactions').post(addReaction);
+
 module.exports = router;
