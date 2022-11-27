594e46c80d46e946b03c307e562629f803ea72f8
sbullocks <sbullocks@gmail.com>
2022-11-26 19:48:47 -0500

    added route for adding a friend.

--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -16,4 +16,6 @@ router.route('/').get(getUsers).post(createUser);
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+router.route('/:userId/friends').post(addFriend);
+
 module.exports = router;
